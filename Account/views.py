be4d46178964305e8892ad7d08fb63d5ff4b078b
samuel mwangi <samuelmwangi729@gmail.com>
2025-10-10 13:55:25 +0300

    feat:override the post method to do the tokens

--- Account/views.py
+++ Account/views.py
@@ -1,6 +1,8 @@
+from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import status
+from rest_framework.response import Response
 
 
 class CustomTokenSerializerPair(TokenObtainPairSerializer):
@@ -13,3 +15,8 @@ class CustomTokenSerializerPair(TokenObtainPairSerializer):
         return token 
 class CustomTokenObtainView(TokenObtainPairView):
     serializer_class = CustomTokenSerializerPair
+    # override the post method here 
+    def post(self,request,*args,**kwargs):
+        response = super().post(request,*args,**kwargs)
+        tokens = response.data
+        return Response({"tokens":tokens},status=status.HTTP_200_OK)
