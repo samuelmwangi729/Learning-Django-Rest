a192743657700b7b5a875b305f3e9264c87e6766
samuel mwangi <samuelmwangi729@gmail.com>
2025-10-10 05:51:53 +0300

    feat:created the login view with token authentication

--- Account/urls.py
+++ Account/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path 
 
+from .views import CustomTokenObtainView 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
-urlpatterns=[
-
-    
+urlpatterns = [
+    path('login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('custom', CustomTokenObtainView.as_view(), name='custom_token'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- Account/views.py
+++ Account/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import status
 
-# Create your views here.
+
+class CustomTokenSerializerPair(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['name'] = user.first_name
+
+        return token 
+class CustomTokenObtainView(TokenObtainPairView):
+    serializer_class = CustomTokenSerializerPair
